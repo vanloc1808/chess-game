44ada7a2af0bc83f914202aca3c4e775fa6cc14e
vanloc1808 <76842571+vanloc1808@users.noreply.github.com>
2021-12-06 14:42:00 +0700

    demo

--- ChessGame/ChessGame/main.cpp
+++ ChessGame/ChessGame/main.cpp
@@ -6,6 +6,22 @@ int main() {
 
 	cb.createChessBoard();
 
+	cout << "Initialization:\n";
+
+	cb.showToConsole();
+
+	system("pause");
+
+	for (int i = 0; i < 16; i++) {
+		cb.movePieceToNewPlace(cb._blackPieces->_pieces[i], cb._blackPieces->_pieces[i]->getCurrentPlace().getColumn(), cb._blackPieces->_pieces[i]->getCurrentPlace().getRow() + 1);
+		cb.movePieceToNewPlace(cb._whitePieces->_pieces[i], cb._whitePieces->_pieces[i]->getCurrentPlace().getColumn(), cb._whitePieces->_pieces[i]->getCurrentPlace().getRow() - 1);
+	}
+
+
+	cb.updateChessBoard();
+
+	cout << "After updating:\n";
+
 	cb.showToConsole();
 
 	return 0;
