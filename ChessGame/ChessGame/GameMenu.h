69b071d2efd1813432b7dda76174bfc535b61a40
vanloc1808 <76842571+vanloc1808@users.noreply.github.com>
2021-12-12 22:47:12 +0700

    declare GameMenu

---
+++ ChessGame/ChessGame/GameMenu.h
@@ -0,0 +1,68 @@
+#ifndef _GAMEMENU_H_
+#define _GAMEMENU_H_
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "Mouse.h"
+#include "EventInput.h"
+#include "GameMusic.h"
+#include "GameSound.h"
+#include "GameWindow.h"
+
+using namespace std;
+using namespace sf;
+
+enum class MenuStatus {
+	WAIT,
+	PLAYING,
+	OPTION,
+	EXITING,
+};
+
+enum class GameMode {
+	PvP,
+	PvE,
+	WAIT,
+};
+
+struct Volume {
+	int sound;
+	int music;
+};
+
+class GameMenu {
+private:
+	Volume _volume;
+
+	GameMouse _menuMouse;
+
+	GameMusic _menuMusic;
+
+	GameSound _menuSound;
+
+	MenuStatus _menuStatus;
+
+	GameWindow _gameWindow;
+
+	EventInput _eventInput;
+
+	GameMode _gameMode;
+
+public:
+	GameMenu();
+
+	~GameMenu();
+
+	void initMouseMusicSound(); //initialize mouse, music and sound
+
+	MenuStatus initMenu(); //initialize menu
+
+	GameMode getGameMode();
+
+	void dragAndDrop(Sprite& s, bool& isMoving, Vector2i pos);
+
+	void setGameVolume(Volume& vol);
+};
+
+#endif // !_GAMEMENU_H_
+
