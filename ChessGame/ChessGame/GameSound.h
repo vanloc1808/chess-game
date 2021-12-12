6036f0921ce8a7c8b8c0a8e819a088ae622e9354
vanloc1808 <76842571+vanloc1808@users.noreply.github.com>
2021-12-12 18:16:01 +0700

    declare GameSound class

---
+++ ChessGame/ChessGame/GameSound.h
@@ -0,0 +1,43 @@
+#ifndef _GAMESOUND_H_
+#define _GAMESOUND_H_
+
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace sf;
+
+class GameSound {
+private:
+	SoundBuffer _buffer;
+
+	Sound _gameSound;
+
+	string _soundLocation;
+
+	bool _isRepeated;
+
+	int _soundVolume;
+
+public:
+	GameSound(); //constructor
+
+	~GameSound(); //destructor
+
+	void setSoundBuffer(string location);
+
+	void setRepeatedSound();
+
+	void playSound();
+
+	void pauseSound();
+
+	void stopSound();
+
+	void changeSoundVolume(); 
+};
+
+
+#endif // !_GAMESOUND_H_
+
