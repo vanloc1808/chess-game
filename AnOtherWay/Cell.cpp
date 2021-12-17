83960ecaf9f68b692278b594da6f37cb3ee12b69
vanloc1808 <vanloc1808@gmail.com>
2021-12-17 15:11:06 +0700

    implement Cell

---
+++ AnOtherWay/Cell.cpp
@@ -0,0 +1,39 @@
+#include "Cell.h"
+
+using namespace utilities;
+
+bool Cell::getIsChecked() {
+    return this->_isChecked;
+}
+
+void Cell::highlight(bool highlightSlot) {
+    if (highlightSlot == true) {
+        this->_status = CellStatus::HIGHLIGHTED;
+
+        if (this->_piece == nullptr) {
+            //empty cell
+            this->_highlightedCircle.setFillColor(Settings::getHighlightedColor());
+
+            this->_highlightedCircle.setOutlineThickness(0);
+
+            this->_highlightedCircle.setScale(Settings::getEmptyHighlightedScale(), Settings::getEmptyHighlightedScale());        
+        } else {
+            //occupied cell
+            this->_highlightedCircle.setFillColor(Color(0, 0, 0, 0));
+
+            this->_highlightedCircle.setOutlineThickness(Settings::getOccupiedHighlightedThickness());
+
+            this->_highlightedCircle.setScale(Settings::getOccupiedHighlightedScale(), Settings::getOccupiedHighlightedScale());
+        }
+    } else {
+        if (this->_piece == nullptr) {
+            this->_status = CellStatus::EMPTY;
+        } else {
+            this->_status = CellStatus::OCCUPIED;
+        }
+    }
+}
+
+void Cell::setIsChecked(bool check) {
+    this->_isChecked = check;
+}
---
+++ AnOtherWay/Cell.h
@@ -0,0 +1,48 @@
+#ifndef _CELL_H_
+#define _CELL_H_
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <memory>
+#include "Settings.h"
+#include "Piece.h"
+
+using namespace std;
+using namespace sf;
+
+//enum class tell the status of a cell (square) in the board
+enum class CellStatus {
+    EMPTY,
+    OCCUPIED,
+    HIGHLIGHTED,
+};
+
+//"forward" the definition of Piece class in Piece.h 
+class Piece; 
+
+class Cell {
+private:
+    bool _isChecked = false;
+protected:
+
+public:
+    RectangleShape _rect; //it's a square actually
+    RectangleShape _checkedRect;
+
+    CircleShape _highlightedCircle;
+
+    shared_ptr<Piece> _piece; //at most one piece on each cell, so we use the pointer, not the vector
+
+    CellStatus _status;
+
+    bool getIsChecked();
+
+    void highlight(bool highlightSlot);
+
+    void setIsChecked(bool check);
+
+};
+//https://stackoverflow.com/questions/18385418/c-meaning-of-a-statement-combining-typedef-and-typename
+typedef vector<vector<Cell>> CellList; 
+
+#endif // _CELL_H_
